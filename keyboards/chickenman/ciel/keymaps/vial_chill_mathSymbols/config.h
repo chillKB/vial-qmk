7b16e11a790a14ea2cce7f884eb27fea6cad7ed8
chillKB <107592205+chillKB@users.noreply.github.com>
2022-06-18 00:56:40 -0700

    reclaim right alt

--- keyboards/chickenman/ciel/keymaps/vial_chill_mathSymbols/config.h
+++ keyboards/chickenman/ciel/keymaps/vial_chill_mathSymbols/config.h
@@ -12,5 +12,10 @@
  * Defines input modes to be used for unicode inputs. UC_WINC is for WinCompose on
  * Windows. Keycodes UC_MOD and UC_RMOD cycle through these modes and UC_M_XX assigns
  * a specific mode.
+ *
+ * The unicode key is the key used by the OS to trigger unicode input.
  */
 #define UNICODE_SELECTED_MODES UC_WINC, UC_MAC, UC_LNX
+#define UNICODE_KEY_WINC KC_F13
+// #define UNICODE_KEY_MAC KC_LALT
+// #define UNICODE_KEY_LNX LCTL(LSFT(KC_U))
